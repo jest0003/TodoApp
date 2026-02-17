77d496d9d18863964aa97dfeff732670fec8c77d
Jerie <jerie.stolbergrohr@gmail.com>
2026-02-17 17:35:42 +0100

    styling done

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const todoText = document.querySelector("#input_text");
 const todoAmount = document.querySelector("#input_number");
-const todoBtn = document.querySelector("#submit_task");
+const addBtn = document.querySelector("#submit_task");
 const todoArr = [];
 const unliked = document.querySelector(".unliked");
 const liked = document.querySelector(".liked");
 const task_container = document.querySelector(".task_container");
 const done_container = document.querySelector(".done_container");
+const todoBtn = document.querySelector("#to_do");
+const doneBtn = document.querySelector("#done");
+const doneSection = document.querySelector(".done_section");
+const todoSection = document.querySelector(".todo_section");
 
-todoBtn.addEventListener("click", submitTodo)
+doneBtn.addEventListener("click", showDone);
+todoBtn.addEventListener("click", showTodo);
+
+function showDone() {
+    todoSection.style.zIndex ="0";
+    doneSection.style.zIndex = "1";
+    todoBtn.style.backgroundColor = "#FFF4DE";
+    doneBtn.style.backgroundColor = "#FFFBF3";
+    todoBtn.classList.remove("active_tab");
+    doneBtn.classList.add("active_tab");
+}
+
+function showTodo() {
+    todoSection.style.zIndex = "1";
+    doneSection.style.zIndex ="0";
+    doneBtn.style.backgroundColor = "#FFF4DE";
+    todoBtn.style.backgroundColor = "#FFFBF3";
+    doneBtn.classList.remove("active_tab");
+    todoBtn.classList.add("active_tab");
+}
+
+addBtn.addEventListener("click", submitTodo);
 
 function submitTodo () {
     const todoObject = {text:todoText.value, amount:todoAmount.value, done:false, liked:false, id:self.crypto.randomUUID ()}
@@ -51,9 +76,6 @@ function showTaskArr(arr) {
              if (evt.target.classList.contains("taskDone")){
                 console.log("task done clicked", `${elm.id}`);
                 elm.done = true;
-                //flyttes her hen til done fanen
-                //Skal hive fat i id: 
-                console.log("done i if =", `${elm.done}`)
                 filterAndSort ();
             }
            }); 
@@ -73,6 +95,7 @@ arr.forEach (elm => {
         <img class="icon undoTask" src="file.png" alt="" /></div>`
 
     done_container.appendChild(li);
+    console.log ("done list id=", `${elm.id}`)
 
     li.addEventListener("click", (evt)=> {
         if (evt.target.classList.contains("undoTask")){
